dd236c949639da73644839778f37f907610dd7ec
Ryan Williams <rwilliams@mla.org>
2017-03-28 16:41:16 -0400

    move global super-admin auto-login out of plugin

--- humanities-commons.php
+++ humanities-commons.php
@@ -1261,15 +1261,6 @@ class Humanities_Commons {
 			return '';
 		}
 
-		// auto-login global super-admins
-		// this is necessary when the super-admin does not have an explicit role on the current network
-		if ( ! is_user_logged_in() && in_array( $user->user_login, $global_super_admins ) ) {
-			$this->hcommons_set_shibboleth_based_user_meta( $user );
-			wp_set_auth_cookie( $user_id );
-			wp_safe_redirect( remove_query_arg( 'reauth', $_SERVER['REQUEST_URI'] ) );
-			exit();
-		}
-
 		//Check for existing user role, we don't want to overwrite role assignments made in WP.
 		global $wp_roles;
 		$user_role_set = false;
