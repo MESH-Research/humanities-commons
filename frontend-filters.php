c2b736450d0c3f9c1c20d8c45ba4705f38cef937
Mike Thicke <thickemi@msu.edu>
2023-02-02 18:24:11 +0000

    commons-474-fix-author-link-filtering

--- frontend-filters.php
+++ frontend-filters.php
@@ -8,9 +8,7 @@
  * frontend.
  *
  * This is intended to alter the display of an author's name in the post_author
- * block. There doesn't appear to be a way of checking whether we are in that
- * context specifically, so this function tries to check for some cases where we
- * might not want to alter the author's display name.
+ * block. 
  * 
  * @see https://github.com/MESH-Research/hastac-migration/issues/92
  *
@@ -26,16 +24,19 @@ function hc_add_post_author_profile_link( $value, $user_id, $original_user_id ) 
 		return $value;
 	}
 
-	// In the context where this is meant to be called,
-	// get_the_author_display_name should be the only active filter. If there
-	// are other nested filters, we probably don't want to do anything.
-	if ( count( $wp_current_filter ) > 1 ) {
-		return $value;
+	// Yuck. But this seems to be the only way to check that we are in the post author block.
+	$calling_functions = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 10);
+	$in_post_author_block = false;
+	foreach ( $calling_functions as $caller ) {
+		if ( array_key_exists( 'function', $caller ) && $caller['function'] === 'render_block_core_post_author' ) {
+			$in_post_author_block = true;
+			break;
+		}
 	}
 
-	// Avoid conflicts if some other filter is adding links or other markup to 
-	// the author's display name.
-	if ( $value !== strip_tags( $value ) ) {
+	// We only want this to apply to the post author block, not other uses of
+	// author_display_name. In those cases, should filter on author_link instead.
+	if ( ! $in_post_author_block ) {
 		return $value;
 	}
 
@@ -45,3 +46,29 @@ function hc_add_post_author_profile_link( $value, $user_id, $original_user_id ) 
 	return $value;
 }
 add_filter ( 'get_the_author_display_name', 'hc_add_post_author_profile_link', 10, 3 );
+
+/**
+ * Replaces link to author's posts on user sites with link to author's profile page,
+ * which also includes their posts. This makes legacy themes match the behavior of
+ * the post author block as filtered by hc_add_post_author_profile_link.
+ *
+ * @see https://github.com/MESH-Research/commons/issues/474
+ * @see https://developer.wordpress.org/reference/functions/get_author_posts_url/ 
+ * 
+ * @param string $link            The current link
+ * @param int    $author_id       The WPID of the author
+ * @param string $author_nicename The author's display name (unused)
+ */
+function hc_filter_author_link( $link, $author_id, $author_nicename ) {
+	if ( ! $author_id ) {
+		return $link;
+	}
+	
+	$profile_url = bp_core_get_user_domain( $author_id );
+	if ( $profile_url ) {
+		return $profile_url;
+	}
+
+	return $link;
+}
+add_filter( 'author_link', 'hc_filter_author_link', 10, 3 );
